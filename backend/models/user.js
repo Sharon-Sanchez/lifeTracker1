d5cdb30fd1eb29d899929ea49ec2cfd527dc71ee
Sharon Sanchez <sharonsanchez581@gmail.com>
2021-06-29 09:55:35 -0700

    Updated backend and set up login

---
+++ backend/models/user.js
@@ -0,0 +1,25 @@
+const { UnauthorizedError, UnauthorizedError } = require("../utils/errors")
+
+class User {
+    static async login(credentials) {
+        // user should submit their email and password
+        // if any of these fields are missing, throw an error
+        //
+        // lookup the user in the db by email
+        // with pass in db
+        //if there is a match
+        //
+        // if any of this goes wrong, throw an error
+        throw new UnauthorizedError("Invalid email/password")
+    }
+
+    static async register(credentials) {
+        // user should submit their email and pass
+        // if any of these fields are missing throw an error
+        //take user pass and hash it 
+        //take email and lowercase
+        //create user in db and return user
+    }
+}
+
+module.exports = User
