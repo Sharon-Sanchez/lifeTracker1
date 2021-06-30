7ab8cdebb7197a09ae95be08cd9b225961a400fe
Sharon Sanchez <sharonsanchez581@gmail.com>
2021-06-30 10:10:42 -0700

    working registration system

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,4 @@
-const { UnauthorizedError } = require("../utils/errors")
+const { UnauthorizedError, BadRequestError } = require("../utils/errors")
 const db = require("../db")
 class User {
     static async login(credentials) {
@@ -14,12 +14,80 @@ class User {
     }
 
     static async register(credentials) {
-        // user should submit their email and pass
-        // if any of these fields are missing throw an error
-        //take user pass and hash it 
-        //take email and lowercase
-        //create user in db and return user
+        const requiredFields = ["email", "username", "firstName", "lastName", "password", "isAdmin"]
+        requiredFields.forEach((property) => {
+            if (!credentials?.hasOwnProperty(property)) {
+              throw new BadRequestError(`Missing ${property} in request body.`)
+            }
+        })
+
+        if (credentials.email.indexOf("@") <= 0) {
+            throw new BadRequestError("Invalid email.")
+        }
+        
+        const existingUser = await User.fetchUserByEmail(credentials.email)
+        if (existingUser) {
+          throw new BadRequestError(`A user already exists with email: ${credentials.email}`)
+        }
+
+        const existingUserWithUsername = await User.fetchUserByUsername(credentials.username)
+        if (existingUserWithUsername) {
+          throw new BadRequestError(`A user already exists with username: ${credentials.username}`)
+        }
+
+        const normalizedEmail = credentials.email.toLowerCase()
+        const normalizedUsername = credentials.username.toLowerCase()
+
+
+        const userResult = await db.query(
+            `INSERT INTO users (email, username, first_name, last_name, password, is_admin)
+             VALUES ($1, $2, $3, $4, $5, $6)
+             RETURNING id, email, username, first_name, last_name, is_admin, created_at;
+            `,
+            [
+              normalizedEmail,
+              normalizedUsername,
+              credentials.firstName,
+              credentials.lastName,
+              credentials.password,
+              credentials.isAdmin
+              
+            ]
+        )
+        const user = userResult.rows[0]
+        return user
+
     }
+
+    static async fetchUserByEmail(email) {
+        if (!email) {
+          throw new BadRequestError("No email provided")
+        }
+    
+        const query = `SELECT * FROM users WHERE email = $1`
+    
+        const result = await db.query(query, [email.toLowerCase()])
+    
+        const user = result.rows[0]
+    
+        return user
+    }
+
+    static async fetchUserByUsername(username) {
+        if (!username) {
+          throw new BadRequestError("No username provided")
+        }
+    
+        const query = `SELECT * FROM users WHERE username = $1`
+    
+        const result = await db.query(query, [username])
+    
+        const user = result.rows[0]
+    
+        return user
+    }
+    
+
 }
 
 module.exports = User
